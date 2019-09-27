1140f85112d28da38d74b21aedec0a446ff1bc3e
BeatrizV11 <beatrizvasconcelos11@gmail.com>
2019-09-27 14:50:27 -0300

    HomeWork 02/10

--- src/test/java/curso/treinamento/steps/VisualizacaoMenuSteps.java
+++ src/test/java/curso/treinamento/steps/VisualizacaoMenuSteps.java
@@ -1,23 +1,34 @@
 package curso.treinamento.steps;
 
+import org.junit.Assert;
+
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
+import curso.treinamento.pages.HomePage;
 
 public class VisualizacaoMenuSteps {
 	
 	@Dado("que eu esteja na tela Home")
 	public void que_eu_esteja_na_tela_Home() {
-	  System.out.println("teste");  
+	  
+		Assert.assertTrue("Página Principal não encontrada", HomePage.validar_pagina());  
 	}
 
-	@Quando("acesso o menu Cars")
-	public void acesso_o_menu_Cars() {
-	    
+	@Quando("acesso o menu Accounts")
+	public void acesso_o_menu_Accounts() {
+		
+		HomePage.clicarBtnAccounts();
+		//Assert.assertTrue("Menu Accounts não encontrado", HomePage.validar_menu_accounts());
 	}
 
-	@Então("são apresentados os itens {string}, {string} e {string}")
-	public void são_apresentados_os_itens_e(String string, String string2, String string3) {
+	@Então("são apresentados os itens {string}, {string}, {string} e {string}")
+	public void são_apresentados_os_itens_e(String string, String string2, String string3, String string4) {
 	
+		Assert.assertTrue("Botão Admins não encontrado", HomePage.validar_btn_admins());
+		Assert.assertTrue("Botão Suppliers não encontrado", HomePage.validar_btn_suppliers());
+		Assert.assertTrue("Botão Customers não encontrado", HomePage.validar_btn_customers());
+		Assert.assertTrue("Botão GuestCustomers não encontrado", HomePage.validar_btn_guestcustomers());
+		
 	}    
 }
--- src/test/resources/features/Visualização_menu.feature
+++ src/test/resources/features/Visualização_menu.feature
@@ -7,8 +7,8 @@
 		Quando faço o login com o usuário "admin@phptravels.com" e senha "demoadmin"
 	
 	@Menu
-	Cenário: Itens apresentados no menu Cars
+	Cenário: Itens apresentados no menu Accounts
 		Dado que eu esteja na tela Home
-		Quando acesso o menu Cars
-		Então são apresentados os itens "Cars", "Extras" e "Cars Settings"
+		Quando acesso o menu Accounts
+		Então são apresentados os itens "Admins", "Suppliers", "Customers" e "Guestcustomers"
 	
